a9d7f156bb203272ee3f7486789dae50e33b921b
Brett Memsic <memsic@gmail.com>
2012-09-07 17:51:45 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodbee::Application.config.secret_token = '0e3c3d0a99e75d6eec2845a5a8ac01584f03bed17f35a99b2cece2153b774667d8f44d300bebe0e898f3a1e13e3fdcfa162f8e58244704d45552c46d78e717ad'
