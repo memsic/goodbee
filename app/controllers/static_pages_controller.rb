049952dca6305c5b0c9341eb93f8cbf158b3ac67
Brett Memsic <memsic@gmail.com>
2012-09-22 20:58:31 -1000

    Add user causes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
  
   def home
     if signed_in?
-      @micropost = current_user.microposts.build
+      @cause = current_user.causes.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
